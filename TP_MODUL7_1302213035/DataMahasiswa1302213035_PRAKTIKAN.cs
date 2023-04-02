9e2108861843e861d85d2a33dbdda0f614cb9f5f
nadhifaaffiyan <127294844+nadhifaaffiyan@users.noreply.github.com>
2023-04-03 01:09:55 +0700

    DataMahasiswa1302213035_PRAKTIKAN

---
+++ TP_MODUL7_1302213035/DataMahasiswa1302213035_PRAKTIKAN.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+class DataMahasiswa1302213035_PRAKTIKAN
+{
+    public class Mahasiswa
+    {
+        public NamaLengkap nama { get; set; }
+
+        public int nim { get; set; }
+
+        public string fakultas { get; set; }
+
+        public Mahasiswa() { }
+
+        public Mahasiswa(NamaLengkap nama, int nim, string fakultas)
+        {
+            this.nama = nama;
+            this.nim = nim;
+            this.fakultas = fakultas;
+        }
+    }
+
+    public class NamaLengkap
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+
+        public NamaLengkap() { }
+
+        public NamaLengkap(string depan, string belakang)
+        {
+            this.depan = depan;
+            this.belakang = belakang;
+        }
+    }
+
+    public void ReadJSON()
+    {
+        string file = "C:\\Users\\asus\\Documents\\TUGAS ADEK\\SEMESTER 4\\KPL\\TP\\tpmodul7_group5\\TP_MODUL7_1302213035\\DataMahasiswa1302213035_PRAKTIKAN.json";
+        string JSON = File.ReadAllText(file);
+        Mahasiswa d = JsonSerializer.Deserialize<Mahasiswa>(JSON);
+        Console.WriteLine($"Nama {d.nama.depan + " " + d.nama.belakang} dengan NIM {d.nim} dari Fakultas {d.fakultas}");
+    }
+}
--- TP_MODUL7_1302213035/Program.cs
+++ TP_MODUL7_1302213035/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TP_MODUL7_1302213035
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa1302213035_PRAKTIKAN dataMahasiswa = new DataMahasiswa1302213035_PRAKTIKAN();
+            dataMahasiswa.ReadJSON();
+
+            Console.ReadKey();
+        }
+    }
+}
